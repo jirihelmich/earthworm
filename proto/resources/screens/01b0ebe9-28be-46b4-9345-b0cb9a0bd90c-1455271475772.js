65ab21083bfb7adc965fead4547f1a550bd22b2f
Jiří Helmich <jiri@helmich.cz>
2016-02-12 11:10:26 +0100

    proto

---
+++ proto/resources/screens/01b0ebe9-28be-46b4-9345-b0cb9a0bd90c-1455271475772.js
@@ -0,0 +1,31 @@
+jQuery("#simulation")
+  .on("click", ".s-01b0ebe9-28be-46b4-9345-b0cb9a0bd90c .click", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    jFirer = jEvent.getEventFirer();
+    if(jFirer.is("#s-Button_2")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/d12245cc-1680-458d-89dd-4f0d7fb22724"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    }
+  });
